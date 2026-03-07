e1419041bcb977a8c8e06e6432f969b98374db5e
nvkhang00 <nvkhang0099@gmail.com>
2026-03-07 21:35:52 +0700

    get dataitem by dataset id & delete dataitem by id API

--- src/DataLabeling.API/Controllers/DataItemController.cs
+++ src/DataLabeling.API/Controllers/DataItemController.cs
@@ -47,6 +47,49 @@ namespace DataLabeling.API.Controllers
             return Ok(MapToResponse(entity));
         }
 
+        [HttpGet("dataset/{datasetId}")]
+        public async Task<IActionResult> GetByDataset(int datasetId)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var dataset = await _context.Datasets
+                .Include(d => d.Project)
+                .FirstOrDefaultAsync(d =>
+                    d.DatasetId == datasetId &&
+                    d.Project.ManagerId == userId);
+
+            if (dataset == null)
+                return BadRequest("Dataset not found or not yours");
+
+            var items = await _context.DataItems
+                .Where(i => i.DatasetId == datasetId)
+                .Select(i => MapToResponse(i))
+                .ToListAsync();
+
+            return Ok(items);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var item = await _context.DataItems
+                .Include(i => i.Dataset)
+                .ThenInclude(d => d.Project)
+                .FirstOrDefaultAsync(i =>
+                    i.ItemId == id &&
+                    i.Dataset.Project.ManagerId == userId);
+
+            if (item == null)
+                return NotFound("DataItem not found");
+
+            _context.DataItems.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return Ok("Deleted successfully");
+        }
+
         private static DataItemResponse MapToResponse(DataItem entity)
         {
             return new DataItemResponse
