2a024fc483a2fe50859800276d32bf5da3a37fa0
nvkhang00 <nvkhang0099@gmail.com>
2026-03-09 20:35:19 +0700

    add create task & get task by round id

---
+++ src/DataLabeling.API/DTOs/TaskResponse.cs
@@ -0,0 +1,16 @@
+﻿using DataLabeling.Entities;
+namespace DataLabeling.API.DTOs
+{
+    public class TaskResponse
+    {
+        public int TaskId { get; set; }
+        public int DatasetRoundId { get; set; }
+        public int AssigneeUserId { get; set; }
+        public TaskType Type { get; set; }
+        public Entities.TaskStatus Status { get; set; }
+        public int GroupNumber { get; set; }
+        public int? ParentTaskId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
