b2d4526ef200cc2f6c22c9d63b4030839b0430cc
nvkhang00 <nvkhang0099@gmail.com>
2026-03-19 22:51:50 +0700

    modify UpdateUserRequest

--- src/DataLabeling.API/DTOs/UpdateUserRequest.cs
+++ src/DataLabeling.API/DTOs/UpdateUserRequest.cs
@@ -5,9 +5,5 @@ namespace DataLabeling.API.DTOs
     public class UpdateUserRequest
     {
         public string FullName { get; set; }
-
-        public UserRole Role { get; set; }
-
-        public string Status { get; set; }
     }
 }
