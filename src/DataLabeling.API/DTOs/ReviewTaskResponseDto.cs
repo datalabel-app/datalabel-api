f9959a655abb532484d1337bdcdfa2db0de90e4f
nvkhang00 <nvkhang0099@gmail.com>
2026-03-22 22:43:33 +0700

    modify some API in task controller

---
+++ src/DataLabeling.API/DTOs/ReviewTaskResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DataLabeling.API.DTOs
+{
+    public class ReviewTaskResponseDto
+    {
+        public int TaskId { get; set; }
+        public int RoundId { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+        public List<ReviewItemDto> Items { get; set; } = new();
+    }
+}
---
+++ src/DataLabeling.API/DTOs/TaskResponseDto.cs
@@ -0,0 +1,19 @@
+﻿namespace DataLabeling.API.DTOs
+{
+    public class TaskResponseDto
+    {
+        public int TaskId { get; set; }
+
+        public string RoundName { get; set; } = string.Empty;
+
+        public string? AnnotatorName { get; set; }
+
+        public DataLabeling.Entities.TaskStatus? Status { get; set; }
+
+        public string? ReviewerName { get; set; }
+
+        public int DataItemCount { get; set; }
+
+        public int ShapeType { get; set; }
+    }
+}
